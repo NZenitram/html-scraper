23231381f935de6248d538c297241adf8287fe77
nzenitram <nicholasmartinez@gmail.com>
2017-08-13 10:47:03 -0600

    working on DB returns

--- get_client.py
+++ get_client.py
@@ -8,11 +8,11 @@ class GetClient:
     def __init__(self, provider):
         self.provider = provider
         self.con = mysql.connector.connect(**config['config']['db'])
-        self.cur = self.con.cursor(dictionary=True)
+        self.cur = self.con.cursor(dictionary=True, buffered=True)
 
     def get_provider_information(self):
         prov_id = str(config['providers'][self.provider])
-        self.cur.execute('SELECT * FROM providers WHERE id =' + prov_id)
+        self.cur.execute('SELECT * FROM provider WHERE id =' + prov_id)
 
         client = [Client(**client) for client in self.cur.fetchall()][0]
 
--- scraper.py
+++ scraper.py
@@ -12,12 +12,43 @@ class Scraper():
         driver.get('http://www.google.com')
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         self.save_html_scraping(soup.prettify(), client)
+        driver.quit()
+
+    def check_if_provider_and_page_record_exists(self, client):
+        check_provider = ("SELECT * FROM provider_page WHERE provider_id=" + str(self.provider.id))
+
+        client.cur.execute(check_provider)
+        return client.cur.fetchone()
+
+
+        return True
+    def check_if_current_scrape_exists(self, client):
+        check_record = ("SELECT current_scrape FROM provider_page WHERE provider_id=" + str(self.provider.id))
+
+        client.cur.execute(check_record)
+        return client.cur.fetchone()
+
+
 
     def save_html_scraping(self, data, client):
-        add_record = ("INSERT INTO provider_pages "
+        add_record = ("INSERT INTO provider_page "
                       "(provider_id, current_scrape, new_scrape, updated_at) "
                       "VALUES (%s, %s, %s, %s)")
 
-        new_scrape = (self.provider.id, None, data, datetime.now() )
+        update_record = ("UPDATE provider_page "
+                         "(provider_id, current_scrape, new_scrape, updated_at) "
+                         "VALUES (%s, %s, %s, %s)"
+                         "WHERE provider_id=" + str(self.provider.id))
 
-        client.cur.execute(add_record, new_scrape)
+        current_scrape = (self.provider.id, data, None, datetime.now())
+        new_scrape = (self.provider.id, None, data, datetime.now())
+
+
+        if not self.check_if_provider_and_page_record_exists(client):
+            client.cur.execute(add_record, current_scrape)
+            client.con.commit()
+
+        if self.check_if_current_scrape_exists(client):
+            client.cur.execute(update_record, new_scrape)
+            client.con.commit()
+
